f5443d746ec77441c82e2f49dc052938a32c8598
Muhammad Bilal <mbilal254.m@gmail.com>
2023-10-31 19:00:31 +0500

    Future and Stream Builder

--- lib/screens/future_and_stream.dart
+++ lib/screens/future_and_stream.dart
@@ -8,10 +8,58 @@ class FutureAndStream extends StatefulWidget {
 }
 
 class _FutureAndStreamState extends State<FutureAndStream> {
+// FutureBuilder
+  Future<int> futureCounter(int counter) async {
+    await Future.delayed(const Duration(seconds: 2));
+    return counter + counter;
+  }
+
+// Stream Builder
+
+  Stream<int> streamCounter(int counter) async* {
+    while (true) {
+      await Future.delayed(Duration(seconds: 1));
+      yield counter++;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(),
-    );
+        appBar: AppBar(
+          title: const Text("Future and Stream Builder"),
+        ),
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              FutureBuilder(
+                  future: futureCounter(10),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return CircularProgressIndicator();
+                    }
+                    return Text(
+                      "Future: ${snapshot.data}",
+                      style: const TextStyle(fontSize: 30),
+                    );
+                  }),
+// Stream Builder
+              StreamBuilder(
+                  stream: streamCounter(10),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return LinearProgressIndicator();
+                    }
+                    return Text(
+                      "Stream ${snapshot.data}",
+                      style: const TextStyle(fontSize: 30),
+                    );
+                  }),
+            ],
+          ),
+        ));
   }
 }
